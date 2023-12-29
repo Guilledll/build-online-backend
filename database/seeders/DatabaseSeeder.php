94ec8f09db6f862cc0bf276772e09cb4db885ed2
Guilledll <guilledll20@gmail.com>
2023-12-29 01:14:46 +0000

    remove seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,6 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // string: password
         ]);
 
-        Contact::factory(10)->for(User::first())->create();
+        // Contact::factory(10)->for(User::first())->create();
     }
 }
